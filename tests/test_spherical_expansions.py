3acc3e503a22c0465b6d23a472cd3ce57ac33bce
Alexander Goscinski <alex.goscinski@posteo.de>
2023-06-19 17:18:11 +0200

    change equals in tests to allclose
    We will change the dtypes of torch tensors to the default type which is
    usually 32 bit. To ignore the accuracy loss of the tests, we change
    equal to allclose

--- tests/test_spherical_expansions.py
+++ tests/test_spherical_expansions.py
@@ -24,14 +24,16 @@ class TestSphericalExpansion:
         vector_expansion = VectorExpansion(self.hypers, device="cpu")
         with torch.no_grad():
             tm = vector_expansion.forward(self.structures)
-        assert equistore.operations.equal(tm_ref, tm)
+        # default types are float32 so we set accuracy to 1e-7
+        equistore.operations.allclose(tm_ref, tm, atol=1e-7, rtol=1e-7)
 
     def test_spherical_expansion_coeffs(self):
         tm_ref = equistore.core.io.load_custom_array("tests/data/spherical_expansion_coeffs-ethanol1_0-data.npz", equistore.core.io.create_torch_array)
         spherical_expansion_calculator = SphericalExpansion(self.hypers, self.all_species)
         with torch.no_grad():
             tm = spherical_expansion_calculator.forward(self.structures)
-        assert equistore.operations.equal(tm_ref, tm)
+        # default types are float32 so we set accuracy to 1e-7
+        equistore.operations.allclose(tm_ref, tm, atol=1e-7, rtol=1e-7)
 
     def test_spherical_expansion_coeffs_alchemical(self):
         with open("tests/data/expansion_coeffs-ethanol1_0-alchemical-hypers.json", "r") as f:
@@ -41,5 +43,5 @@ class TestSphericalExpansion:
         spherical_expansion_calculator = SphericalExpansion(hypers, self.all_species)
         with torch.no_grad():
             tm = spherical_expansion_calculator.forward(self.structures)
-        assert equistore.operations.equal(tm_ref, tm)
-
+        # default types are float32 so we set accuracy to 1e-7
+        equistore.operations.allclose(tm_ref, tm, atol=1e-7, rtol=1e-7)
