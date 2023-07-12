b9e773e605086faca71fc3752804406c50e473df
frostedoyster <bigi.f@libero.it>
2023-07-12 04:53:14 +0200

    Normalization scheme for MLPs

--- torch_spex/radial_basis.py
+++ torch_spex/radial_basis.py
@@ -1,6 +1,7 @@
 import torch
 import equistore
 from .le import get_le_spliner
+from .normalize import normalize_true, normalize_false
 
 
 class RadialBasis(torch.nn.Module):
@@ -8,30 +9,37 @@ class RadialBasis(torch.nn.Module):
     def __init__(self, hypers, all_species, device) -> None:
         super().__init__()
 
+        if hypers["normalize"]:
+            normalize = normalize_true
+        else:
+            normalize = normalize_false
+
         self.n_max_l, self.spliner = get_le_spliner(hypers["E_max"], hypers["r_cut"], device=device)
         self.l_max = len(self.n_max_l) - 1
         self.radial_transform = (lambda x: x)
         if "alchemical" in hypers:
             self.is_alchemical = True
             self.n_pseudo_species = hypers["alchemical"]
-            self.combination_matrix = torch.nn.Linear(all_species.shape[0], self.n_pseudo_species, bias=False)
+            self.combination_matrix = normalize(torch.nn.Linear(all_species.shape[0], self.n_pseudo_species, bias=False))
             self.all_species_labels = equistore.Labels(
                 names = ["species_neighbor"],
                 values = all_species[:, None]
             )
         else:
             self.is_alchemical = False
-        """self.radial_mlps = torch.nn.ModuleDict({
+        
+        all_species_names = range(self.n_pseudo_species) if "alchemical" in hypers else all_species
+        self.radial_mlps = torch.nn.ModuleDict({
             str(l)+"_"+str(aj) : torch.nn.Sequential(
-                torch.nn.Linear(self.n_max_l[l], 32),
-                torch.nn.SiLU(),
-                torch.nn.Linear(32, 32),
-                torch.nn.SiLU(),
-                torch.nn.Linear(32, 32),
-                torch.nn.SiLU(),
-                torch.nn.Linear(32, self.n_max_l[l])
-            ) for aj in range(self.n_pseudo_species) for l in range(self.l_max+1)
-        })"""
+                normalize(torch.nn.Linear(self.n_max_l[l], 32, bias=False)),
+                normalize(torch.nn.SiLU()),
+                normalize(torch.nn.Linear(32, 32, bias=False)),
+                normalize(torch.nn.SiLU()),
+                normalize(torch.nn.Linear(32, 32, bias=False)),
+                normalize(torch.nn.SiLU()),
+                normalize(torch.nn.Linear(32, self.n_max_l[l], bias=False))
+            ) for aj in range(all_species_names) for l in range(self.l_max+1)
+        })
 
     def forward(self, r, samples_metadata):
 
@@ -56,9 +64,11 @@ class RadialBasis(torch.nn.Module):
             radial_basis.append(radial_basis_l)
             index += self.n_max_l[l]
 
-        """for l in range(self.l_max+1):
+        for l in range(self.l_max+1):
             for aj in range(self.n_pseudo_species):
-                radial_basis[l][:, aj, :] = self.radial_mlps[str(l)+"_"+str(aj)](radial_basis[l][:, aj, :].clone())"""
+                radial_basis[l][:, aj, :] = self.radial_mlps[str(l)+"_"+str(aj)](radial_basis[l][:, aj, :].clone())
+
+        # NEED APPLY OPTION FOR NON-ALCHEMICAL MODEL
 
         return radial_basis
 
