2d1961047177b91b2c8776cb5328465ff34bd939
Alexander Goscinski <alex.goscinski@posteo.de>
2023-07-14 18:41:29 +0200

    add sorting to tests of vector expansion
    * ase.neighborlist can have different order on different systems for
    unknown reasons so we need to sort the output according to the metadata
    before comparing

--- tests/test_spherical_expansions.py
+++ tests/test_spherical_expansions.py
@@ -5,6 +5,7 @@ import pytest
 import torch
 
 import equistore
+from equistore import Labels, TensorBlock, TensorMap
 import numpy as np
 import ase.io
 
@@ -21,9 +22,12 @@ class TestSphericalExpansion:
 
     def test_vector_expansion_coeffs(self):
         tm_ref = equistore.core.io.load_custom_array("tests/data/vector_expansion_coeffs-ethanol1_0-data.npz", equistore.core.io.create_torch_array)
+        # we need to sort both computed and reference pair expansion coeffs,
+        # because ase.neighborlist can get different neighborlist order for some reasons
+        tm_ref = sort_tm(tm_ref)
         vector_expansion = VectorExpansion(self.hypers, self.all_species, device="cpu")
         with torch.no_grad():
-            tm = vector_expansion.forward(self.structures)
+            tm = sort_tm(vector_expansion.forward(self.structures))
         # Default types are float32 so we cannot get higher accuracy than 1e-7.
         # Because the reference value have been cacluated using float32 and
         # now we using float64 computation the accuracy had to be decreased again
@@ -60,3 +64,39 @@ class TestSphericalExpansion:
         # Because the reference value have been cacluated using float32 and
         # now we using float64 computation the accuracy had to be decreased again
         assert equistore.operations.allclose(tm_ref, tm, atol=1e-5, rtol=1e-5)
+
+### these util functions will be removed once lab-cosmo/equistore/pull/281 is merged
+def native_list_argsort(native_list):
+    return sorted(range(len(native_list)), key=native_list.__getitem__)
+
+def sort_tm(tm):
+    blocks = []
+    for _, block in tm.items():
+        values = block.values
+
+        samples_values = block.samples.values
+        sorted_idx = native_list_argsort([tuple(row.tolist()) for row in block.samples.values])
+        samples_values = samples_values[sorted_idx]
+        values = values[sorted_idx]
+
+        components_values = []
+        for i, component in enumerate(block.components):
+            component_values = component.values
+            sorted_idx = native_list_argsort([tuple(row.tolist()) for row in component.values])
+            components_values.append( component_values[sorted_idx] )
+            values = np.take(values, sorted_idx, axis=i+1)
+
+        properties_values = block.properties.values
+        sorted_idx = native_list_argsort([tuple(row.tolist()) for row in block.properties.values])
+        properties_values = properties_values[sorted_idx]
+        values = values[..., sorted_idx]
+
+        blocks.append(
+            TensorBlock(
+                values=values,
+                samples=Labels(values=samples_values, names=block.samples.names),
+                components=[Labels(values=components_values[i], names=component.names) for i, component in enumerate(block.components)],
+                properties=Labels(values=properties_values, names=block.properties.names)
+            )
+        )
+    return TensorMap(keys=tm.keys, blocks=blocks)
