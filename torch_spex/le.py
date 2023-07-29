Fix radial normalization issues

torch_spex/le.py
@@ -69,7 +69,8 @@ def get_le_spliner(E_max, r_cut, normalize, device):
             R[i] = R_nl(n, el, r[i])
         return_array = N_nl(n, el) * R * r_cut ** (-1.5)
         if normalize:
-            return_array *= np.sqrt( (4/3)*np.pi*r_cut**3 ) # normalize by square root of sphere volume
+            # normalize by square root of sphere volume, excluding sqrt(4pi) which is included in the SH
+            return_array *= np.sqrt( (1/3)*r_cut**3 )
         return return_array
 
     normalization_check_integral, _ = sp.integrate.quadrature(
@@ -79,7 +80,7 @@ def get_le_spliner(E_max, r_cut, normalize, device):
         maxiter = 200
     )
     if normalize:
-        normalization_check_integral /= (4/3)*np.pi*r_cut**3
+        normalization_check_integral /= (1/3)*r_cut**3
     if abs(normalization_check_integral - 1) > 1e-6:
         raise ValueError("normalization of radial basis FAILED")
 