Fix radial normalization issues

torch_spex/physical_le.py
@@ -186,7 +186,9 @@ def get_physical_le_spliner(E_max, r_cut, r0, normalize, cost_trade_off, device)
                 ret += coeffs[l][m, n]*b(l, m, 1-np.exp(-r/r0), z_nl, precomputed_N_nl)
             else:
                 ret += coeffs[l][m, n]*b(l, m, (1-np.exp(-r/r0))*(1.0-np.exp(-(r/rnn)**2)), z_nl, precomputed_N_nl)
-        if normalize: ret *= np.sqrt( (4/3)*np.pi*r_cut**3 ) # normalize by square root of sphere volume
+        if normalize:
+            # normalize by square root of sphere volume, excluding sqrt(4pi) which is included in the SH
+            ret *= np.sqrt( (1/3)*r_cut**3 )
         return ret
 
     def function_for_splining_derivative(n, l, r):
@@ -196,7 +198,9 @@ def get_physical_le_spliner(E_max, r_cut, r0, normalize, cost_trade_off, device)
                 ret += coeffs[l][m, n]*db(l, m, 1-np.exp(-r/r0), z_nl, precomputed_N_nl)*np.exp(-r/r0)/r0
             else:
                 ret += coeffs[l][m, n]*db(l, m, (1-np.exp(-r/r0))*(1.0-np.exp(-(r/rnn)**2)), z_nl, precomputed_N_nl)*((1.0-np.exp(-(r/rnn)**2))*np.exp(-r/r0)/r0+(1-np.exp(-r/r0))*np.exp(-(r/rnn)**2)*2*r/rnn**2)
-        if normalize: ret *= np.sqrt( (4/3)*np.pi*r_cut**3 ) # normalize by square root of sphere volume
+        if normalize:
+            # normalize by square root of sphere volume, excluding sqrt(4pi) which is included in the SH
+            ret *= np.sqrt( (1/3)*r_cut**3 )
         return ret
 
     if rs:

torch_spex/spherical_expansions.py
@@ -76,6 +76,7 @@ class SphericalExpansion(torch.nn.Module):
         if self.normalize:
             avg_num_neighbors = hypers["normalize"]
             self.normalization_factor = 1.0/np.sqrt(avg_num_neighbors)
+            self.normalization_factor_0 = 1.0/avg_num_neighbors**(3/4)
         self.all_species = np.array(all_species, dtype=np.int32)  # convert potential list to np.array
         self.vector_expansion_calculator = VectorExpansion(hypers, self.all_species, device=device)
 
@@ -146,7 +147,11 @@ class SphericalExpansion(torch.nn.Module):
                 where_ai = torch.LongTensor(np.where(ai_new_indices == a_i)[0]).to(densities_l.device)
                 densities_ai_l = torch.index_select(densities_l, 0, where_ai)
                 if self.normalize:
-                    densities_ai_l *= self.normalization_factor
+                    if l == 0:
+                        # Very high correlations for l = 0: use a stronger normalization
+                        densities_ai_l *= self.normalization_factor_0
+                    else:
+                        densities_ai_l *= self.normalization_factor
                 labels.append([a_i, l, 1])
                 blocks.append(
                     TensorBlock(